0891662820fb667fc98277237311d1f98f6dbce1
nulldeba <nulldebashis@gmail.com>
2018-11-15 18:31:31 +0530

    Added 'renderDish' function for task-2(Assignment1)

--- src/components/DishDetailsComponent.js
+++ src/components/DishDetailsComponent.js
@@ -30,18 +30,28 @@ class DishDetail extends Component {
         else
             return (<div></div>);
     }
+
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else
+            return (<div></div>);
+    }
     render() {
         if (this.props.dish != null)
             return (
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
-                            <CardBody>
-                                <CardTitle>{this.props.dish.name}</CardTitle>
-                                <CardText>{this.props.dish.description}</CardText>
-                            </CardBody>
-                        </Card>
+                        {this.renderDish(this.props.dish)}
                     </div>
                     <div className="col-12 col-md-5 m-1">
                         {this.renderComments(this.props.dish.comments)}
