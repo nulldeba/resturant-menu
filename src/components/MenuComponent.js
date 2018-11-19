a5c6e83474547f66cc94e898211a86048cfbfc07
nulldeba <nulldebashis@gmail.com>
2018-11-19 19:32:07 +0530

    React Router

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 
-function RenderMenuItem({dish, onClick})
+function RenderMenuItem({dish})
 {
     return(
         <Card key={dish.id}
-                        onClick={() =>onClick(dish.id)} style={{ cursor: 'pointer' }}>
+                        style={{ cursor: 'pointer' }}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
